afd84a0c3ac62b90d27aae59e0c3e434cf1fb694
ShakaPursuit <shakalivngstone@pursuit.org>
2023-11-16 14:01:46 -0500

    added psql command

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors= require('cors');
-const morgan = require("morgan");
+// const morgan = require("morgan");
 
 const app= express();
 
 //Middle Ware
 app.use(cors());
 app.use(express.json());
-app.use(morgan("tiny"));
+// app.use(morgan("tiny"));
 
 //Accessing Router and Setting paths
  app.get("/",(req,res)=>{res.send('Welcome to Music Connection!')})
